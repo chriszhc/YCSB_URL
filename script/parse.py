2f12b19ceb14aac18bf785eb3825f715792d35d6
Huanchen Zhang <huanchenzhang@yahoo.com>
2013-10-22 18:30:57 -0400

    url mapping done!

--- script/parse.py
+++ script/parse.py
@@ -1,5 +1,7 @@
 import sys
 
+num_urls = 1000
+
 f=open("../urls/small_1000.txt", 'r')
 count = 0
 urls = {}
@@ -14,11 +16,11 @@ f.close()
 for line in sys.stdin :
     cols = line.split()
     if (cols[0] == "SCAN") or (cols[0] == "INSERT") :
-        firstUrl = int(cols[2][4:]) % 1000
+        firstUrl = int(cols[2][4:]) % num_urls
         if cols[0] == "SCAN" :
             numUrl = int(cols[3])
-            lastUrl = (firstUrl + numUrl - 1) % 1000
-            print cols[0], cols[1], urls[firstUrl], urls[lastUrl], numUrl
+            lastUrl = (firstUrl + numUrl - 1) % num_urls
+            print cols[0], urls[firstUrl], urls[lastUrl], numUrl
         else :
-            print cols[0], cols[1], urls[firstUrl]
+            print cols[0], urls[firstUrl]
 
