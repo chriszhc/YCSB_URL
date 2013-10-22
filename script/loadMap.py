8f65eaf0a874faa6988f72d43bded90eb7d7beae
Huanchen Zhang <huanchenzhang@yahoo.com>
2013-10-22 00:51:15 -0400

    sync

--- script/loadMap.py
+++ script/loadMap.py
@@ -1,12 +1,16 @@
 import sys
 
-inputFile = sys.argv[1]
-outputFile = sys.argv[2]
+inputURL = sys.argv[1]
+inputLoad = sys.argv[2]
+inputTxn = sys.argv[3]
+outputLoad = sys.argv[4]
+outputTxn = sys.argv[5]
 
 try:
     urls = open(inputFile, 'r')
     url_list = urls.readlines();
     urls.close()
+    
     int_url_map = open(outputFile, 'w')
 except IOError, e:
     print 'Cannot open file'
