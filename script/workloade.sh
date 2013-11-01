edee8a0cae68b7c236f9cf2b10f398fb6abddcd0
Huanchen Zhang <huanchenzhang@yahoo.com>
2013-10-31 20:00:24 -0400

    large workload

---
+++ script/workloade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#YCSB="~/Documents/Research/YCSB"
+#YCSB_URL="~/Documents/Research/YCSB_URL"
+
+cd ~/Documents/Research/YCSB
+
+./bin/ycsb load basic -P ./workloads/workloade -p recordcount=1000000 -s > ~/Documents/Research/YCSB_URL/output_ycsb/loade_1M.dat
+
+./bin/ycsb run basic -P ./workloads/workloade -p recordcount=1000000 -s > ~/Documents/Research/YCSB_URL/output_ycsb/txnse_1M.dat
+
