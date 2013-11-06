b47770b9887cd1bfb43bd801232975b46fe83be4
Huanchen Zhang <huanchenzhang@Huanchens-MacBook-Pro.local>
2013-11-06 09:23:23 -0500

    deleted workloade.sh

--- script/workloade.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~/Documents/YCSB
-
-./bin/ycsb load basic -P ./workloads/workloade -P ../YCSB_URL/script/small_1K.dat -s > ~/Documents/Research/YCSB_URL/output_ycsb/loade_1K.dat
-
-./bin/ycsb run basic -P ./workloads/workloade -P ../YCSB_URL/script/small_1K.dat -s > ~/Documents/Research/YCSB_URL/output_ycsb/txnse_1K.dat
-
-
